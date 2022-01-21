473ee7d1efb3d708687c1fdeba78365a8543790f
roggllau <93657667+roggllau@users.noreply.github.com>
2022-01-21 10:22:27 +0100

    roger commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello DAM1!!");
+        System.out.println("Guifré Agustín");
         System.out.println("Hola Guifré i Tomàs! Soc el Roger.");
     }
 }
